31f3c73f02816d85499452b341a7253d9e4b2e16
fish2k <burhanoff1992@gmail.com>
2019-01-21 18:06:22 +0300

    responsive

--- js/main.js
+++ js/main.js
@@ -14,13 +14,34 @@ $(document).ready(function(){
 
   product.each(function(){
     $(this).owlCarousel({
-      items: 5,
+      items: 1,
       margin: 15,
       nav: true,
       navText: [
         '<img src="../img/product-arl.jpg">',
         '<img src="../img/product-arr.jpg">'
-      ]
+      ],
+      responsive: {
+        991: {
+          items: 5
+        },
+        768: {
+          items: 4
+        },
+        640: {
+          items: 3
+        },
+        340: {
+          items: 2
+        }
+      }
     });
+  });
+
+  var burger = $('.header-burger');
+  var nav = $('.header-nav');
+
+  burger.click(function(){
+    nav.toggleClass('show-nav');
   });
 });
