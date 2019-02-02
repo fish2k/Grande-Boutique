21104768b036a90806e10243f051b8c1e8bc00e9
Burhanov Ildar <burhanoff1992@gmail.com>
2019-02-02 11:55:12 +0300

    moschino

--- js/main.js
+++ js/main.js
@@ -41,6 +41,23 @@ $(document).ready(function(){
     });
   });
 
+  var aboutSlider = $('.about-slider');
+
+  aboutSlider.owlCarousel({
+    items: 2,
+    margin: 30,
+    nav: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      475: {
+        items: 2
+      }
+    }
+  });
+
   // Бургер меню
   var burger = $('.header-burger');
   var nav = $('.header-nav');
@@ -128,6 +145,90 @@ $(document).ready(function(){
     sub.stop();
     sub.slideToggle(300);
     $(this).toggleClass('show-sub');
+  });
+
+
+  var sync1 = $(".product-photos");
+  var sync2 = $(".product-thumbs");
+  var slidesPerPage = 4;
+  var syncedSecondary = true;
+
+  sync1.owlCarousel({
+    items : 1.35,
+    center: true,
+    stagePadding: 30,
+    slideSpeed : 2000,
+    smartSpeed: 700,
+    nav: false,
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsiveRefreshRate: 200,
+    responsive: {
+      0: {
+        items: 1,
+        stagePadding: 0
+      },
+      600: {
+        items : 1.35
+      }
+    }
+  }).on('changed.owl.carousel', syncPosition);
+
+  sync2
+    .on('initialized.owl.carousel', function () {
+      sync2.find(".owl-item").eq(0).addClass("current");
+    })
+    .owlCarousel({
+    items : slidesPerPage,
+    dots: false,
+    nav: true,
+    smartSpeed: 700,
+    slideSpeed : 700,
+    margin: 30,
+    slideBy: slidesPerPage,
+    responsiveRefreshRate : 100
+  }).on('changed.owl.carousel', syncPosition2);
+
+  function syncPosition(el) {
+    var count = el.item.count-1;
+    var current = Math.round(el.item.index - (el.item.count/2) - .5);
+    
+    if(current < 0) {
+      current = count;
+    }
+    if(current > count) {
+      current = 0;
+    }
+    
+    sync2
+      .find(".owl-item")
+      .removeClass("current")
+      .eq(current)
+      .addClass("current");
+    var onscreen = sync2.find('.owl-item.active').length - 1;
+    var start = sync2.find('.owl-item.active').first().index();
+    var end = sync2.find('.owl-item.active').last().index();
+    
+    if (current > end) {
+      sync2.data('owl.carousel').to(current, 100, true);
+    }
+    if (current < start) {
+      sync2.data('owl.carousel').to(current - onscreen, 100, true);
+    }
+  }
+  
+  function syncPosition2(el) {
+    if(syncedSecondary) {
+      var number = el.item.index;
+      sync1.data('owl.carousel').to(number, 100, true);
+    }
+  }
+  
+  sync2.on("click", ".owl-item", function(e){
+    e.preventDefault();
+    var number = $(this).index();
+    sync1.data('owl.carousel').to(number, 300, true);
   });
 
 });
